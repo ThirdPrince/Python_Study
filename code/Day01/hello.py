0141f6e4939b5c732b2193bcbe0a05d62f9c4d28
wujun <1297260647@qq.com>
2023-03-21 20:07:59 +0800

    Python_Study

---
+++ code/Day01/hello.py
@@ -0,0 +1,9 @@
+"""
+first code
+Author:dhl
+Date:2023 3.21
+
+基本的print输出 和Java差不多
+"""
+print('hello,world')
+print('你好，世界')
---
+++ code/Day02/var1.py
@@ -0,0 +1,12 @@
+"""
+Author:dhl
+Date:2023 3.21
+基本变量的使用
+"""
+a = 321
+b = 123
+
+print(a + b)
+print(a - b)
+print(a * b)
+
